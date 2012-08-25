eb52bb42d94a25633245dd93d772644954e35b59
Jeff Grimes <jeffgrimes9@gmail.com>
2012-08-25 13:33:34 -0700

    getting started

--- PersistentData/ViewController.m
+++ PersistentData/ViewController.m
@@ -8,27 +8,18 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)viewDidUnload
-{
+- (void)viewDidUnload {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+- (void)dealloc {
+    [super dealloc];
 }
 
 @end
