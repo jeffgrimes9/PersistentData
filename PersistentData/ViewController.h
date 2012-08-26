78bd626b79a7f5143f1bcbb089c4c35a88e0f1f5
Jeff Grimes <jeffgrimes9@gmail.com>
2012-08-26 11:36:04 -0700

    final touches

--- PersistentData/ViewController.h
+++ PersistentData/ViewController.h
@@ -10,4 +10,23 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, retain) IBOutlet UITextField *textField;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *segmentedControl1;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *segmentedControl2;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *segmentedControl3;
+@property (nonatomic, retain) IBOutlet UISlider *slider;
+@property (nonatomic, retain) IBOutlet UISwitch *switch1;
+@property (nonatomic, retain) IBOutlet UISwitch *switch2;
+@property (nonatomic, retain) IBOutlet UISwitch *switch3;
+@property (nonatomic, retain) IBOutlet UIStepper *stepper1;
+@property (nonatomic, retain) IBOutlet UIStepper *stepper2;
+@property (nonatomic, retain) IBOutlet UILabel *stepper1Label;
+@property (nonatomic, retain) IBOutlet UILabel *stepper2Label;
+
+- (IBAction)hideKeyboard;
+- (IBAction)stepper1DidChange;
+- (IBAction)stepper2DidChange;
+- (IBAction)storeData;
+- (IBAction)fetchData;
+
 @end
--- PersistentData/ViewController.m
+++ PersistentData/ViewController.m
@@ -12,6 +12,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.stepper1.minimumValue = 0;
+    self.stepper1.maximumValue = 99;
+    self.stepper2.minimumValue = 0;
+    self.stepper2.maximumValue = 99;
+}
+
+- (IBAction)hideKeyboard {
+    [self.textField resignFirstResponder];
+}
+
+- (IBAction)stepper1DidChange {
+    int stepperValue = (int)self.stepper1.value;
+    self.stepper1Label.text = [NSString stringWithFormat:@"%i", stepperValue];
+}
+
+- (IBAction)stepper2DidChange {
+    int stepperValue = (int)self.stepper2.value;
+    self.stepper2Label.text = [NSString stringWithFormat:@"%i", stepperValue];
+}
+
+- (IBAction)storeData {
+    NSString *string = self.textField.text;
+    NSNumber *control1 = [NSNumber numberWithInt:(int)self.segmentedControl1.selectedSegmentIndex];
+    NSNumber *control2 = [NSNumber numberWithInt:(int)self.segmentedControl2.selectedSegmentIndex];
+    NSNumber *control3 = [NSNumber numberWithInt:(int)self.segmentedControl3.selectedSegmentIndex];
+    NSNumber *slider = [NSNumber numberWithFloat:self.slider.value];
+    BOOL switch1 = self.switch1.on;
+    BOOL switch2 = self.switch2.on;
+    BOOL switch3 = self.switch3.on;
+    NSNumber *stepper1 = [NSNumber numberWithInt:(int)self.stepper1.value];
+    NSNumber *stepper2 = [NSNumber numberWithInt:(int)self.stepper2.value];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [defaults setObject:string forKey:@"string"];
+    [defaults setObject:control1 forKey:@"control1"];
+    [defaults setObject:control2 forKey:@"control2"];
+    [defaults setObject:control3 forKey:@"control3"];
+    [defaults setObject:slider forKey:@"slider"];
+    [defaults setBool:switch1 forKey:@"switch1"];
+    [defaults setBool:switch2 forKey:@"switch2"];
+    [defaults setBool:switch3 forKey:@"switch3"];
+    [defaults setObject:stepper1 forKey:@"stepper1"];
+    [defaults setObject:stepper2 forKey:@"stepper2"];
+    
+    [defaults synchronize];
+}
+
+- (IBAction)fetchData {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    NSString *string = [defaults stringForKey:@"string"];
+    NSNumber *control1 = [defaults objectForKey:@"control1"];
+    NSNumber *control2 = [defaults objectForKey:@"control2"];
+    NSNumber *control3 = [defaults objectForKey:@"control3"];
+    NSNumber *slider = [defaults objectForKey:@"slider"];
+    BOOL switch1 = [defaults boolForKey:@"switch1"];
+    BOOL switch2 = [defaults boolForKey:@"switch2"];
+    BOOL switch3 = [defaults boolForKey:@"switch3"];
+    NSNumber *stepper1 = [defaults objectForKey:@"stepper1"];
+    NSNumber *stepper2 = [defaults objectForKey:@"stepper2"];
+    
+    self.textField.text = string;
+    self.segmentedControl1.selectedSegmentIndex = [control1 integerValue];
+    self.segmentedControl2.selectedSegmentIndex = [control2 integerValue];
+    self.segmentedControl3.selectedSegmentIndex = [control3 integerValue];
+    self.slider.value = [slider floatValue];
+    self.switch1.on = switch1;
+    self.switch2.on = switch2;
+    self.switch3.on = switch3;
+    self.stepper1.value = (double)[stepper1 intValue];
+    self.stepper2.value = (double)[stepper2 intValue];
+    self.stepper1Label.text = [NSString stringWithFormat:@"%i", (int)self.stepper1.value];
+    self.stepper2Label.text = [NSString stringWithFormat:@"%i", (int)self.stepper2.value];
 }
 
 - (void)viewDidUnload {
